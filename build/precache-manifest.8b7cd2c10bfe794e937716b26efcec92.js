0a8664a9c7c6218da072c52bdc3039c711fb1e10
adnan bashir <adnanbashir3358@gmail.com>
2022-09-24 13:09:47 +0400

    minor

---
+++ build/precache-manifest.8b7cd2c10bfe794e937716b26efcec92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81d4845b4bf4be85787530a3649c45b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ca628b6f6d4a616093",
+    "url": "/static/css/main.355f5b51.chunk.css"
+  },
+  {
+    "revision": "345debc96e19a770ecf0",
+    "url": "/static/js/2.764ec48e.chunk.js"
+  },
+  {
+    "revision": "5514ecf99bae0d3f5e7938b8776d66bb",
+    "url": "/static/js/2.764ec48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ca628b6f6d4a616093",
+    "url": "/static/js/main.ae7c77fb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ae7c77fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  }
+]);
